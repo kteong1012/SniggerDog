ba96a2db47c08030260e479c64863f6d089159dc
carson <396098651@qq.com>
2022-08-24 00:41:00 +0800

    feat: 整理服务器代码结构

--- Server/ServerStartUp/Program.cs
+++ Server/ServerStartUp/Program.cs
@@ -1,46 +1,9 @@
-﻿using System;
-using System.IO;
-using System.Threading;
-using TouchSocket.Core.Dependency;
-using TouchSocket.Core.Log;
-
-namespace PostMainland // Note: actual namespace depends on the project name.
+﻿namespace PostMainland // Note: actual namespace depends on the project name.
 {
     public class Program
     {
-
-        static IContainer _container;
-        static ThreadSynchronizationContext ThreadSynchronizationContext = ThreadSynchronizationContext.Instance;
-        static TimeInfo TimeInfo = TimeInfo.Instance;
         static void Main(string[] args)
         {
-
-            SynchronizationContext.SetSynchronizationContext(ThreadSynchronizationContext.Instance);
-            Console.WriteLine(Directory.GetCurrentDirectory());
-            Log.SetLogs(new FileLogger(), new ConsoleLogger(LogType.Warning, LogType.Error));
-            Global.Container = new Container()
-                .RegisterSingleton<IConfigLoader, ConfigLoader>()
-                .RegisterSingleton<IAssemblyManager, AssemblyManager>()
-                .RegisterSingleton<IProtocalManagerService, ProtocalManager>()
-                .RegisterTransient<TcpServerService, TcpServerService>();
-
-            _container = Global.Container;
-            _container.Resolve<IConfigLoader>();
-            _container.Resolve<TcpServerService>();
-
-            while (true)
-            {
-                try
-                {
-                    Thread.Sleep(1);
-                    Update();
-                    LateUpdate();
-                }
-                catch (Exception e)
-                {
-                    Log.Error(e);
-                }
-            }
         }
     }
 }
